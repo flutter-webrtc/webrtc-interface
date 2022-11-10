43e16edc8b2ee39aa6bdcc386e7ae4d5b060eca9
Dima Rostopira <rostopiradv@gmail.com>
2022-11-10 13:09:52 +0300

    [WIP] Add MediaRecorder for iOS and OSX
    Don't merge yet! Work in progress.
    PR will be ready soon.

--- lib/src/media_recorder.dart
+++ lib/src/media_recorder.dart
@@ -3,12 +3,16 @@ import 'media_stream.dart';
 import 'media_stream_track.dart';
 
 abstract class MediaRecorder {
-  /// For Android use audioChannel param
-  /// For iOS use audioTrack
+  /// Starts recording to file at [path].
+  /// Optionally, on Android choose [audioChannel] to record.
+  /// Optionally, on iOS/OSX set [audioTrack] to record. Only remote audio
+  /// tracks are supported!
+  /// On web platform use [startWeb] instead.
   Future<void> start(
     String path, {
     MediaStreamTrack? videoTrack,
     RecorderAudioChannel? audioChannel,
+    MediaStreamTrack? audioTrack,
   });
 
   /// Only for Flutter Web
